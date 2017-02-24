d14f69f80262bd0123cc7950bb8d140f5992f74a
chaim-aharonson <chaim.aharonson@gmail.com>
2017-02-24 17:34:04 +0200

    another version

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,147 +1,107 @@
+/*!
+ * @ Package for Responsive Boilerplate Micro Lib @!!!!!
+ */
 module.exports = function(grunt) {
+    "use strict";
 
-  // Project configuration.
-  grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
-    watch: {
-      coffee: {
-        files: ['assets/coffeescript/{,*/}*.coffee'],
-        tasks: ['coffee:dev']
-      },
-      compass: {
-        files: ['assets/sass/{,*/}*.{scss,sass}'],
-        tasks: ['compass:dev', 'cssmin:dev']
-      }
-    },
-    connect: {
-      dev: {
-        options: {
-          port: 9001,
-          base: ''
-        }
-      },
-      prod: {
-        options: {
-          port: 9001,
-          base: '_site/',
-          keepalive: true
-        }
-      }
-    },
-    coffee: {
-      dev: {
-        expand: true,
-        flatten: true,
-        cwd: 'assets/coffeescript/',
-        src: ['*.coffee'],
-        dest: 'assets/js/',
-        ext: '.js'
-      }
-    },
-    uglify: {
-      options: {
-        mangle: true
-      },
-      dev: {
-          expand: true,                      // Enable dynamic expansion.
-          cwd: 'assets/js/',                 // Src matches are relative to this path.
-          src: ['*.js', '!*.min.js'],        // Actual pattern(s) to match.
-          dest: 'assets/js/',                // Destination path prefix.
-          ext: '.min.js'                     // Dest filepaths will have this extension.
-        }
-    },
-    compass: {
-      dev: {
-        options: {
-          cssDir: "assets/css",
-          sassDir: "assets/sass",
-          imagesDir: "assets/img",
-          javascriptsDir: "assets/js"
-        }
-      }
-    },
-    cssmin: {
-      dev: {
-        expand: true,
-        cwd: 'assets/css/',
-        src: ['*.css', '!*.min.css', '!_*.css'],
-        dest: 'assets/css/',
-        ext: '.min.css'
-      }
-    },
-    clean: {
-      site: '_site'
-    },
-    copy: {
-      html: {
-        expand: true,
-        cwd: '',
-        src: ['*.html', '*/*.html'],
-        dest: '_site/',
-        flatten: false,
-        filter: 'isFile'
-      },
-      css: {
-        expand: true,
-        cwd: 'assets/css',
-        src: ['*.min.css', '_*.css'],
-        dest: '_site/assets/css',
-        flatten: true,
-        filter: 'isFile'
-      },
-      js: {
-        expand: true,
-        cwd: 'assets/js',
-        src: '*.min.js',
-        dest: '_site/assets/js',
-        flatten: true,
-        filter: 'isFile'
-      },
-      img: {
-        expand: true,
-        cwd: 'assets/img',
-        src: '*',
-        dest: '_site/assets/img',
-        flatten: true,
-        filter: 'isFile'
-      }
-    },
-    imagemin: {
-      prod: {
-        files: [{
-            expand: true,
-            cwd: 'assets/img',
-            src: '{,*/}*.{png,jpg,jpeg}',
-            dest: 'assets/img'
-        }]
-      }
-    },
-    htmlrefs: {
-      prod: {
-        expand: true,
-        cwd: '_site',
-        src: ['*.html', '*/*.html'],
-        dest: '_site/',
-        flatten: true,
-        filter: 'isFile'      
-      }
-    }
-  });
+    // Time to execute each task, you can see at the end, after Grunt command on terminal
+    require('time-grunt')(grunt);
+    // Loading all Grunt-tasks module
+    require('load-grunt-tasks')(grunt);
 
-  // Load the plugins that provide the tasks
-  grunt.loadNpmTasks('grunt-contrib-coffee');
-  grunt.loadNpmTasks('grunt-contrib-compass');
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-contrib-cssmin');
-  grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-contrib-copy');
-  grunt.loadNpmTasks('grunt-contrib-clean');
-  grunt.loadNpmTasks('grunt-contrib-connect');
-  grunt.loadNpmTasks('grunt-contrib-imagemin');
-  grunt.loadNpmTasks('grunt-htmlrefs');
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		// Copyright, Version number and date.
+		banner: '/*!' +
+			'* <%= pkg.name %> v<%= pkg.version %> - <%= grunt.template.today("yyyy-mm-dd") %>. \n' +
+			'* Author: <%= pkg.author %> - Licensed under MIT license.\n' +
+			'*/\n',
+		// Begining of tasks          
+		// Starting lint JavaScript files
+		jshint: {
+			options: {
+				browser: true,
+				globals: {
+					jQuery: true,
+					console: true,
+					module: true,
+					document: true,
+                    browser: true
+				}
+			},
+			all: {
+				files: {
+					src: ['gruntfile.js', 'js/custom.js']
+				}
+			}
+		},
+        uglify: {
+            options: {
+                banner: '<%= banner %>'
+            },
+            files: {
+                src: [ 'js/*.js' ],
+                dest: 'js/',
+                expand: true,
+                flatten: true,
+                ext: '.min.js'
+            }
+        },
+		recess: {
+			options: {
+				compile: true,
+				banner: '<%= banner %>'
+			},
+			base: {
+				src: ['less/responsiveboilerplate.less'],
+				dest: 'css/<%= pkg.name %>.css'
+			},
+			min: {
+				options: {
+					compress: true
+				},
+				src: ['less/responsiveboilerplate.less'],
+				dest: 'css/<%= pkg.name %>.min.css'
+			}
+		},
+        express: {
+            all: {
+                options: {
+                    bases: ['.'],
+                    port: 3001,
+                    hostname: "0.0.0.0",
+                    livereload: true
+                }
+            }
+        },
+		watch: {
+			scripts: {
+				files: ['Gruntfile.js', 'js/**/*.js', 'libs/**/*.js', 'css/**/*.css'],
+				tasks: ['jshint','uglify'],
+				options: {
+					debounceDelay: 250
+				}
+			},
+			recess: {
+				files: 'less/*.less',
+				tasks: ['recess']
+			}
+		},
+        open: {
+            all: {
+                path: 'http://localhost:3001/demo.html'
+            }
+        }
+	});
 
-  // Default task(s)
-  grunt.registerTask('dev', ['connect:dev', 'watch']);
-  grunt.registerTask('prod', ['uglify', 'imagemin', 'clean', 'copy', 'htmlrefs', 'connect:prod']);
+    // Loading Grunt-tasks from node_modules folder
+    grunt.loadTasks('tasks');
 
+    // Register tasks for development and production
+	grunt.registerTask('default', ['jshint','uglify','recess']);
+	// Register tasks for development using watch
+    grunt.registerTask('dev', ['watch']);
+
+    // Register task to see the Demo page using localhost:3001
+    grunt.registerTask('demo', ['express','open','watch']);
 };
